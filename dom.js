e6e1b9954f0265f7669978266e8c9929a6ffda08
adarshgautam <adarshgautam531@gmail.com>
2024-07-07 10:55:57 +0530

    DOM

---
+++ dom.js
@@ -0,0 +1,7 @@
+console.log(document.querySelector('button'));
+
+document.querySelector('.change').innerHTML = 'changed';
+
+const change = document.querySelector('.change');
+
+console.log(change)
