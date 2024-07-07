8466dd47d549b4b31e4d324ac6612ef1ab62fa63
adarshgautam <adarshgautam531@gmail.com>
2024-07-07 21:12:43 +0530

    added css in rock paper and scissores

--- RockPaperScissorsDom.js
+++ RockPaperScissorsDom.js
@@ -12,42 +12,42 @@ function computerMove(){
     const randNum = Math.random();
 
     if (randNum > 0 && randNum < 1/3){
-        comChoice = 'Rock';
+        comChoice = 'rock';
     }else if (randNum >= 1/3 && randNum < 2/3){
-        comChoice = 'Paper';
+        comChoice = 'paper';
     }else{
-        comChoice = 'Scissors';
+        comChoice = 'scissors';
     }
     return comChoice;
 }
 
 function playGame(playerMove){
     const pickcomputerMove = computerMove();
-    let result = 'hello';
-    if (playerMove === 'Scissors'){
-        if (pickcomputerMove === 'Rock'){
+    let result = '';
+    if (playerMove === 'scissors'){
+        if (pickcomputerMove === 'rock'){
             result = 'You lose';
-        }else if (pickcomputerMove === 'Paper'){
+        }else if (pickcomputerMove === 'paper'){
             result = 'You win';
         }else {
             result = 'Tie';
         }
     }
 
-    if (playerMove === 'Rock'){
-        if (pickcomputerMove === 'Rock'){
+    if (playerMove === 'rock'){
+        if (pickcomputerMove === 'rock'){
             result = 'Tie';
-        }else if (pickcomputerMove === 'Paper'){
+        }else if (pickcomputerMove === 'paper'){
             result = 'You lose';
         }else {
             result = 'You win';
         }
     }
 
-    if (playerMove === 'Paper'){
-        if (pickcomputerMove === 'Rock'){
+    if (playerMove === 'paper'){
+        if (pickcomputerMove === 'rock'){
             result = 'You win';
-        }else if (pickcomputerMove === 'Paper'){
+        }else if (pickcomputerMove === 'paper'){
             result = 'Tie';
         }else {
             result = 'You lose';
@@ -56,7 +56,7 @@ function playGame(playerMove){
 
     document.querySelector('.result').innerHTML = result;
 
-    document.querySelector('.choice').innerHTML = `You ${playerMove} and computer ${pickcomputerMove}`
+    document.querySelector('.choice').innerHTML = `You <img src="images/${playerMove}-emoji.png" alt="" class="move-icon"> and <img src="images/${pickcomputerMove}-emoji.png" alt="" class="move-icon"> computer`
 
     if (result === 'You win'){
         score.win += 1;
--- sub.js
+++
@@ -1,9 +0,0 @@
-function Subscribe(){
-    const isSub = document.querySelector('.sub');
-
-    if (isSub.innerText === 'Subscribe'){
-        isSub.innerText = 'Subscribed';
-    } else{
-        isSub.innerText = 'Subscribe';
-    }
-}
