1a447a73ae79b14c3ef6710614ed8de9a5cbbf79
adarshgautam <adarshgautam531@gmail.com>
2024-08-03 14:48:12 +0530

    final todo

--- simple-todo-list.js
+++ simple-todo-list.js
@@ -1,11 +1,22 @@
-const todoList = []
+const todoList = [{
+    listItem: '',
+    date: ''
+}
+];
 
 renderToDoList();
 
 function renderToDoList(){
     let innerToDoHTML = '';
     for (let i = 0; i < todoList.length; i++){
-        let todo = `<p>${todoList[i]}</p>`;
+        let todo =` 
+            <div>${todoList[i]['listItem']}</div>
+            <div>${todoList[i]['date']}</div>
+            <button onclick='
+                todoList.splice(${i}, 1);
+                renderToDoList();
+            ' class='del-btn'>Delete</button>
+        `;
         innerToDoHTML += todo;
     }
 
@@ -16,8 +27,19 @@ function renderToDoList(){
 
 function addTodo(){
     const inputElement = document.querySelector('.to-do-input');
-    const value = inputElement.value;
-    todoList.push(value);
+    const dateInput = document.querySelector('.dateInput');
+
+    const listItem = inputElement.value;
+    const date = dateInput.value;
+
+    todoList.push(
+        {
+            listItem,
+            date
+        }
+    )
+
+
     //console.log(todoList)
     inputElement.value = '';
 
